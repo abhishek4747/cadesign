903256d5706c7877f046e23c0adf73c2118acaae
Nikhil Verma <nikhilverma.ajm@gmail.com>
2014-11-02 17:22:08 +0530

    Question4 fractals complete

---
+++ Class Assignments/fractals.m
@@ -0,0 +1,33 @@
+function fractals(n)
+    points = [0,0;
+              cos(pi/3),sin(pi/3);
+              1,0;
+              0,0];
+                
+    for i = 1:n
+       points = genPoints(points,i);
+    end
+    plotPoints(points);
+end
+
+function morePoints = genPoints(points,n)
+    morePoints = zeros(length(points)*4 - 3,2);
+    
+    for i = 1:length(points)-1
+       A = points(i,:);
+       B = points(i+1,:);
+       morePoints(4*i-3,:) =  A;
+       morePoints(4*i+1,:) =  B;
+       direction = atan2(B(2)-A(2),B(1)-A(1));
+       
+       morePoints(4*i-2,:) = (2*A + B)/3;
+       morePoints(4*i-1,:) = morePoints(4*i-2,:) +...
+           (3^-n)*[cos(direction+pi/3),sin(direction+pi/3)];
+       morePoints(4*i,:) = (A + 2*B)/3;
+    end
+end
+
+function plotPoints(points)
+    figure;
+    plot(points(:,1),points(:,2));
+end
