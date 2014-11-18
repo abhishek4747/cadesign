02117c0c6b3f210b1cdeab35bb7c17e410bbcde3
Abhishek Kumar <abhishek.iitd16@gmail.com>
2014-11-18 13:36:05 +0530

    cubic ui

--- Programming Assignments/cubic_spline/cubic_gui.m
+++ Programming Assignments/cubic_spline/cubic_gui.m
@@ -91,21 +91,21 @@ function numeric_Callback(hObject, eventdata, handles)
 
 function generate_curve(handles)
     %disp(handles)
-    p1 = [str2double(get(handles.p1x,'String')) str2double(get(handles.p1y,'String'))];
-    p1_ = [str2double(get(handles.p1_x,'String')) str2double(get(handles.p1_y,'String'))];
-    p2 = [str2double(get(handles.p2x,'String')) str2double(get(handles.p2y,'String'))];
-    p2_ = [str2double(get(handles.p2_x,'String')) str2double(get(handles.p2_y,'String'))];
+    p1 = [str2double(get(handles.p1x,'String')); str2double(get(handles.p1y,'String'))];
+    p1_ = [str2double(get(handles.p1_x,'String')); str2double(get(handles.p1_y,'String'))];
+    p2 = [str2double(get(handles.p2x,'String')); str2double(get(handles.p2y,'String'))];
+    p2_ = [str2double(get(handles.p2_x,'String')); str2double(get(handles.p2_y,'String'))];
     
     t2 = str2double(get(handles.t2,'String'));
     t = 0:0.01:t2;
     
-    B1 = [p1(1) p1(2)];
-    B2 = [p1_(1) p1_(2)];
-    B3 = [(3*(p2(1)-p1(1)/t2^2 - 2*p1_(1)/t2 - p2_(1)/t2 )) (3*(p2(2)-p1(2)/t2^2 - 2*p1_(2)/t2 - p2_(2)/t2 ))];
-    B4 = [(2*(p1(1)-p2(1))/t2^3 + p1_(1)/t2^2 + p2_(1)/t2^2) (2*(p1(2)-p2(2))/t2^3 + p1_(2)/t2^2 + p2_(2)/t2^2)];
-    x = B1(1) + B2(1)*t + B3(1).*t.*t + B4(1).*t.*t.*t;
-    y = B1(2) + B2(2)*t + B3(2).*t.*t + B4(2).*t.*t.*t;
-    plot(x,y)
+    B1 = p1;
+    B2 = p1_;
+    B3 = (3*(p2-p1/t2^2 - 2*p1_/t2 - p2_/t2 ));
+    B4 = (2*(p1-p2)/t2^3 + p1_/t2^2 + p2_/t2^2);
+    curv = B1*ones(1,length(t)) + B2*t + B3*(t.*t) + B4*(t.*t.*t);
+
+    plot(curv(1,:),curv(2,:));
     hold on;
     plot(p1(1),p1(2),'o');
     plot(p2(1),p2(2),'o');
