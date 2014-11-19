86bc2faf6613e6fbb81213cc7333f12f313e1091
Akhil Jain <akhiljain93>
2014-11-19 14:54:01 +0530

    transforms completed

---
+++ Programming Assignments/transforms/rot.m
@@ -0,0 +1,4 @@
+function T = rot(theta)
+    T = [cos(theta), sin(theta), 0;
+        -sin(theta), cos(theta), 0;
+        0, 0, 1];
---
+++ Programming Assignments/transforms/scale.m
@@ -0,0 +1,4 @@
+function T = scale(s)
+    T = [s, 0, 0;
+        0, s, 0;
+        0, 0, 1];
